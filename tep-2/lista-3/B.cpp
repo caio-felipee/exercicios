72ae7025d859d96be2438490634a2eae7096cc7e
Caio Felipe <caiofeliperro@gmail.com>
2023-05-08 13:52:06 -0300

    new problem solved

--- tep-2/lista-3/B.cpp
+++ tep-2/lista-3/B.cpp
@@ -5,41 +5,39 @@ using namespace std;
 int main(void) {
     int round;
 
-    while(cin >> round and round != -1) {
+    while(cin >> round && round != -1) {
         string solution, guess;
 
         cin >> solution >> guess;
 
+        string ans = "You chickened out.";
+        set<char> sl(solution.begin(), solution.end());
+        set<char> gs;
+        
         int wrong = 0;
-        int state = 0;
 
-        set<char> s(solution.begin(), solution.end());
-        set<char> g;
-
-        cout << "Round " << round << "\n";
-
-        for(auto c : guess) {
-            if(s.find(c) != s.end())
-                g.insert(c);
+        for(auto letter : guess) {
+            if(gs.find(letter) != gs.end())
+                continue;
+            
+            if(sl.find(letter) != sl.end())
+                sl.erase(letter);
             else
                 wrong++;
             
             if(wrong >= 7) {
-                state = 1;
+                ans = "You lose.";
                 break;
             }
 
-            if(g.size() == s.size()) {
-                state = 2;
+            if(!sl.size()) {
+                ans = "You win.";
                 break;
             }
+
+            gs.insert(letter);
         }
 
-        if(not state)
-            cout << "You chickened out." << "\n";
-        else if(state == 1)
-            cout << "You lose." << "\n";
-        else
-            cout << "You win." << "\n";        
+        cout << "Round " << round << "\n" << ans << "\n";
     }
 }
